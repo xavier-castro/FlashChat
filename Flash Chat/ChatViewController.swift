d3491a9c3be852c3e9ccecb177c94940c0dd948d
xavier-castro <xavier_castro@icloud.com>
2019-02-08 15:37:38 -0600

    Add animations to chat

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 
 
-class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     
     // Declare instance variables here
@@ -33,7 +33,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         
         //TODO: Set yourself as the delegate of the text field here:
-
+        messageTextfield.delegate = self
         
         
         //TODO: Set the tapGesture here:
@@ -97,11 +97,25 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     //TODO: Declare textFieldDidBeginEditing here:
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.5) {
+            
+            self.heightConstraint.constant = 315
+            
+            self.view.layoutIfNeeded() // If a constraint has a changed, the layout will redraw
+            
+        }
+        
+    }
     
     
     
     //TODO: Declare textFieldDidEndEditing here:
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
 
     
     ///////////////////////////////////////////
