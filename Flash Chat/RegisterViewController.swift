dc73502c1ec110ad9b7b4b486df3e562f6616c40
xavier-castro <xavier_castro@icloud.com>
2019-02-08 17:06:05 -0600

    Added SVProgressHUD in the RegisterViewController

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 
 class RegisterViewController: UIViewController {
@@ -30,17 +31,24 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject) {
         
-
+        SVProgressHUD.show()
         
         //TODO: Set up a new user on our Firbase database
         Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            
             if error != nil {
+                
                 print(error!)
+                
             } else {
+                
                 print("Registration Successful!")
+                
+                SVProgressHUD.dismiss()
                 
                 // Go to ChatViewController if Registration is successful
                 self.performSegue(withIdentifier: "goToChat", sender: self)
+                
             }
         }
         
